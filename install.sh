c495aaf7f9a721e50a13cf175b50d90c4deb5f3a
JamesSmaldon <James.Smaldon@gmail.com>
2016-01-19 21:45:53 +0000

    Added install script for linux.

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+command -v git >/dev/null 2>&1 || { echo >&2 "Please install git first"; exit 1; }
+command -v curl >/dev/null 2>&1 || { echo >&2 "Please install curl first"; exit 1; }
+
+VIMRC="$HOME/.vimrc"
+
+if [ -e $VIMRC ]
+then
+  read -p "Overwrite existing .vimrc [y/n]? " yn
+  case $yn in
+    [Yy]* ) mv $VIMRC $VIMRC.bak ;;
+    [Nn]* ) echo "Aborting..."; exit 1;; 
+  esac
+fi
+
+echo "Copying new vimrc file."
+cp ./files/vimrc $VIMRC
+
+VUNDLEDIR=$HOME/.vim/bundle
+
+if [ -e $VUNDLEDIR ]
+then
+  echo "Removing existing vundle install"
+  rm -rf $VUNDLEDIR
+fi
+
+echo "Installing vundle."
+mkdir $VUNDLEDIR
+git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLEDIR/Vundle.vim
+
+echo "Installing plugins"
+gvim +PluginInstall +qall
